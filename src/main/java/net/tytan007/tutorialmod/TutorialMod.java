7b0c1aa93715e67e07799655fe800607a7d46563
nestor <elcorreodenestor@gmail.com>
2026-07-28 19:24:00 +0200

    Done video #4 tutorial: [Creative Mode Tab]

--- src/main/java/net/tytan007/tutorialmod/TutorialMod.java
+++ src/main/java/net/tytan007/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.tytan007.tutorialmod;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.tytan007.tutorialmod.block.ModBlocks;
+import net.tytan007.tutorialmod.item.ModCreativeModeTabs;
 import net.tytan007.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -38,6 +39,8 @@ public class TutorialMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/net/tytan007/tutorialmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,40 @@
+package net.tytan007.tutorialmod.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.tytan007.tutorialmod.TutorialMod;
+import net.tytan007.tutorialmod.block.ModBlocks;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+
+    public static final Supplier<CreativeModeTab> DORO_ITEMS_TAB = CREATIVE_MODE_TAB.register("doro_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.DORO_HD.get()))
+                    .title(Component.translatable("creativetab.tytan007tutorialmod.doro_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.BISMUTH);
+                        output.accept(ModItems.DORO_HD);
+                    }).build());
+
+    public static final Supplier<CreativeModeTab> DORO_BLOCKS_TAB = CREATIVE_MODE_TAB.register("doro_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.DORO_HD_BLOCK.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, "doro_items_tab"))
+                    .title(Component.translatable("creativetab.tytan007tutorialmod.doro_blocks"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModBlocks.DORO_BLOCK);
+                        output.accept(ModBlocks.DORO_HD_BLOCK);
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
